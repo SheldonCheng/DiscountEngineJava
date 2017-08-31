21e426a0365f335e52366462f354034761884104
issuser <issuser@ISSCR3127.stf.nus.edu.sg>
2017-08-31 14:40:14 +0800

    Catalog Implementation

---
+++ src/Catalog.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Catalog {
+
+	private ArrayList<Product> CatalogList = new ArrayList<Product>();
+	
+	public Catalog (){
+		
+		// Hard Code Items
+		Add(new Product(1,"alpha"));
+		Add(new Product(2,"beta"));
+		Add(new Product(3,"charlie"));
+		Add(new Product(4,"delta"));
+		Add(new Product(5,"echo"));
+				
+	}
+
+	public void Add (Product newItem) {
+		CatalogList.add(newItem);
+	}
+	
+	public void Remove (Product item) {
+        CatalogList.remove(item);
+	}
+
+	public int getQuantity () {
+		return CatalogList.size();
+	}
+}
---
+++ src/Product.java
@@ -0,0 +1,21 @@
+
+public class Product {
+	
+	public float fPrice;
+	public String sItemCode;
+	
+	public Product (float price, String itemCode)
+	{
+		fPrice = price;
+		sItemCode = itemCode;
+	}
+	
+	public void SetPrice (float price) {
+		fPrice = price;
+	}
+	
+	public float GetPrice () {
+		return fPrice;
+	}
+
+}
